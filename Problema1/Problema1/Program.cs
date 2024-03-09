c73c0908df32da523f7b492676f158f98ae6fe03
fernancat <fernancarranza90@gmail.com>
2024-03-09 11:56:36 -0600

    Problema 1

---
+++ Problema1/Problema1/Program.cs
@@ -0,0 +1,17 @@
+﻿string nombre;
+string correo;
+string[] correo_modificado;
+Console.Write("Ingresa tu nombre: ");
+ nombre = Console.ReadLine();
+Console.WriteLine("---------Bienvenido {0} -----", nombre);
+
+Console.Write("Ingresa un correo de tipo gmail: ");
+correo = Console.ReadLine();
+
+correo_modificado = correo.Split("@gmail.com");
+
+
+Console.WriteLine("Tu correo institucional es: {0}@miumg.edu.gt ", correo_modificado);
+
+Console.ReadKey(true);
+
